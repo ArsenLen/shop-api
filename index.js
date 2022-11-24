c88ed2072b46696971c6e93ea2c65189024eed21
ArsenLen <laurie10@mail.ru>
2022-11-25 01:30:48 +0600

    payment method added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const multer = require("multer")
 const path = require('path')
 const authRoute = require("./routes/auth")
 const cors = require("cors")
-const stripe = require('stripe')('pk_test_51LY447HWg33SQmOYkw5NDamYDIC6nmq6E8TuAzs8BFgElOjFEhM8GjZxjoIguoAhF07s5XgS346RXTd4Fx4xz9rX00cYDOothX')
+const stripe = require('stripe')('sk_test_51LY447HWg33SQmOYNs0iePfJf1zBqa4AM9JFThAVbM5i5Nf6Ph2EYWIUANQpaMpozs9mb4Rnk9WjAo2l7B4Cn9Ca00MBfQmxNQ')
 
 dotenv.config()
 
@@ -36,19 +36,36 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use("/api", productsRoute)
 app.use("/api", authRoute)
 
-app.post('/api/pay', async (req, res) => {
-    const { price, id } = req.body
-    try {
-        const payment = await stripe.paymentIntents.create({
-            amount: price,
-            currency: 'USD',
-            payment_method : id
-        });
-        res.json("success")
-    } catch (err) {
-        console.log(err)
+// app.post('/api/pay', async (req, res) => {
+//     const { price, id } = req.body
+//     try {
+//         const payment = await stripe.paymentIntents.create({
+//             amount: price,
+//             currency: 'USD',
+//             payment_method : id
+//         });
+//         res.json("success")
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
+
+app.post("/api/create-payment-intent", async (req, res) => {
+    
+    const { total } = req.body;
+    // Create a PaymentIntent with the order amount and currency
+    if(total === 0) {
+        return res.status(401).json("Ваша сумма 0")
     }
-})
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total * 100,
+      currency: "usd"
+    });
+  
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    });
+});
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("OK");
